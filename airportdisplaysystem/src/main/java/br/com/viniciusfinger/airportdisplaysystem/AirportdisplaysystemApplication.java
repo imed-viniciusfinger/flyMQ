499338ba4199d8db5445bcdfcf9ebcc022eefd9a
Vinícius Finger <vinicius.finger@imedicina.com.br>
2022-09-14 20:53:30 -0300

    enabling RabbitMQ

--- airportdisplaysystem/src/main/java/br/com/viniciusfinger/airportdisplaysystem/AirportdisplaysystemApplication.java
+++ airportdisplaysystem/src/main/java/br/com/viniciusfinger/airportdisplaysystem/AirportdisplaysystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class AirportdisplaysystemApplication {
 
 	public static void main(String[] args) {
